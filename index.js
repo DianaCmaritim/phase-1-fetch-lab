90a7b5fa1f4ecb29f08500a719327150c2f06b04
Diana <maritimdiane96@gmail.com>
2022-12-14 23:56:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
-//function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+function fetchBooks() {
+  //return fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(books => books.json())
+  .then(data => renderBooks(data))
 
-//}
+}
 
-/*function fetchBooks(books) {
+function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
-}*/
-
- function fetchBooks(){
- }
-
-function renderBooks(json) {
-  const main = document.querySelector('main');
-  json.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2);
-  });
 }
-document.addEventListener('DOMContentLoaded', function(){
-    fetchBooks()
-  })
-const url =("https://anapioficeandfire.com/api/books")
-function fetchBooks(){
-   fetch(url)
-   .then(resp => resp.json())
-   .then(json => renderBooks(json))
-}
-renderBooks(json)
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+  renderBooks(data);
+
+});
+
+
